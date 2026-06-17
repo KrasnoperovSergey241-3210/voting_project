09767b278d8865256f3e8d2f74de45293e8be35e
KrasnoperovSergey241-3210 <sakrasnoperov2006@gmail.com>
2026-06-17 17:39:58 +0300

    Allowed hosts fix 2

--- config/settings.py
+++ config/settings.py
@@ -17,11 +17,7 @@ SECRET_KEY = "django-insecure-44us@==%4vg5xydda30#n0crsuvp4b@kychtr4%2k#^a%6kq5h
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "voting-app-lolr.onrender.com",
-]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
